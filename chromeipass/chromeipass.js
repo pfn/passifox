0732728dfbad0a17487c5f8a0bfbf711861aea68
tomsykes <tom@tomarq.co.uk>
2014-05-20 15:22:31 +0100

    Update chromeipass.js
    Adding input[type=tel] to inputQueryPattern selectors

--- chromeipass/chromeipass.js
+++ chromeipass/chromeipass.js
@@ -789,7 +789,7 @@ cipDefine.prepareStep3 = function() {
 
 cipFields = {}
 
-cipFields.inputQueryPattern = "input[type='text'], input[type='email'], input[type='password'], input:not([type])";
+cipFields.inputQueryPattern = "input[type='text'], input[type='email'], input[type='password'], input[type='tel'], input:not([type])";
 // unique number as new IDs for input fields
 cipFields.uniqueNumber = 342845638;
 // objects with combination of username + password fields
