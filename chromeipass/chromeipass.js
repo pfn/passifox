a1f909c9b79c099cc2c4a5c2ec380fdfcaa537e9
Perry Nguyen <pfnguyen@hanhuy.com>
2016-03-22 16:30:42 -0700

    fix #470 #356 #341 #292 #270
    use event.keyIdentifier (chrome-specific) not keyCode

--- chromeipass/chromeipass.js
+++ chromeipass/chromeipass.js
@@ -61,10 +61,10 @@ chrome.extension.onMessage.addListener(function(req, sender, callback) {
 // ctrl + shift + u = fill username + password
 window.addEventListener("keydown", function(e) {
 	if (e.ctrlKey && e.shiftKey) {
-		if (e.keyCode == 80) { // P
+		if (e.keyIdentifier == "U+0050") { // P
 			e.preventDefault();
 			cip.fillInFromActiveElementPassOnly(false);
-		} else if (e.keyCode == 85) { // U
+		} else if (e.keyIdentifier == "U+0055") { // U
 			e.preventDefault();
 			cip.fillInFromActiveElement(false);
 			var field =_f(cipFields.combinations[0].username);
