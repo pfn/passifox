d1a911cbbf7cd8c54886317135d61abe54702955
Jan Bader <jan.bader@ub-software.de>
2016-05-31 19:35:49 +0200

    Also fire input event (fixes Fastmail)

--- chromeipass/chromeipass.js
+++ chromeipass/chromeipass.js
@@ -1420,6 +1420,7 @@ cip.fillInStringFields = function(fields, StringFields, filledInFields) {
 
 cip.setValueWithChange = function(field, value) {
 	field.val(value);
+	field[0].dispatchEvent(new Event('input', {'bubbles': true}));
 	field[0].dispatchEvent(new Event('change', {'bubbles': true}));
 }
 
