124a01935773b3fc1ca1973c85479e15c8c04ad7
zeromido <zaglolgamal@gmail.com>
2016-08-03 00:25:54 +0200

    Added 'username' input type.
    Added 'username' as a valid input type for credential selection. Fixes #538

--- chromeipass/chromeipass.js
+++ chromeipass/chromeipass.js
@@ -791,7 +791,7 @@ cipDefine.prepareStep3 = function() {
 
 cipFields = {}
 
-cipFields.inputQueryPattern = "input[type='text'], input[type='email'], input[type='password'], input[type='tel'], input[type='number'], input:not([type])";
+cipFields.inputQueryPattern = "input[type='text'], input[type='email'], input[type='username'], input[type='password'], input[type='tel'], input[type='number'], input:not([type])";
 // unique number as new IDs for input fields
 cipFields.uniqueNumber = 342845638;
 // objects with combination of username + password fields
