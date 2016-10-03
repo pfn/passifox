fc86e75261d82db7d9cf80c9c8599d2f90a8792d
iarp <ian@iarp.ca>
2016-10-02 22:45:19 -0400

    If the field to be filled in has a maxlength attribute, use that value to truncate input value.

--- chromeipass/chromeipass.js
+++ chromeipass/chromeipass.js
@@ -1418,6 +1418,15 @@ cip.fillInStringFields = function(fields, StringFields, filledInFields) {
 }
 
 cip.setValueWithChange = function(field, value) {
+
+	var attribute_maxlength = field.attr('maxlength');
+	if (typeof attribute_maxlength !== typeof undefined &&
+		$.isNumeric(attribute_maxlength) === true &&
+		attribute_maxlength > 0) {
+
+		value = value.substr(0, attribute_maxlength);
+	}
+
 	field.val(value);
 	field[0].dispatchEvent(new Event('input', {'bubbles': true}));
 	field[0].dispatchEvent(new Event('change', {'bubbles': true}));
